35da04bfc54222852eb09d32c01624fc1f1345d8
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-05 20:35:46 +0200

    Putting components together

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
-import HomePage from './Components/home_page/home';
+import HomePage from './Components/MainPage';
 
 function App() {
   return (
