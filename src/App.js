9e865e6f9a9e27a4975fa7ab99ba4f8896cb8c31
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-05 20:34:23 +0200

    Implementing FAQ component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import HomePage from './Components/home_page/home';
 
 function App() {
   return (
     <div>
+      <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
