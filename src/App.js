b1dd160427a9e2bf64fe04149f9d7c729bce66d6
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-06 00:29:39 +0200

    Adding routes to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import HomePage from './Components/HomePage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Components/HomePage/Header';
 
 function App() {
   return (
     <div>
       <Router>
+
         <Switch>
-          <Route path='/' exact component={HomePage} />
+          <Route path='/handle' exact component={HomePage} />
+          <Route path='/handle/:handle' exact component={HomePage} />
+          <Route path='/' component={HomePage} />
         </Switch>
+
       </Router>
     </div>
   );
