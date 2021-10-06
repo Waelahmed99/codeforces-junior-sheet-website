3ace756e9f037909827666a331c8b92287442fd2
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-06 14:25:51 +0200

    Adding /handle route componenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HomePage from './Components/HomePage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HandlePage from './Components/HandlePage';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
 
         <Switch>
-          <Route path='/handle' exact component={HomePage} />
+          <Route path='/handle' exact component={HandlePage} />
           <Route path='/handle/:handle' exact component={HomePage} />
           <Route path='/' component={HomePage} />
         </Switch>
---
+++ src/Components/HandlePage/HandlePage.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function HandlePage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default HandlePage
