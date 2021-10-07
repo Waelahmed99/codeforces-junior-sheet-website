dbae6c7b0f62d7a488860f7f647c809d1c445130
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-07 16:16:07 +0200

    Creating problem row component

---
+++ src/Components/ProblemsPage/ProblemRow/ProblemRow.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles.css'
+
+function ProblemRow({ problem }) {
+    return (
+        <tr key={problem.name + problem.contestId}>
+            <td>
+                <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`}>
+                    {problem.name}
+                </a>
+            </td>
+            <td>{problem.verdict}</td>
+            <td>{problem.index}</td>
+            <td>{problem.contestId}</td>
+            <td>{problem.count}</td>
+        </tr>
+    )
+}
+
+export default ProblemRow
