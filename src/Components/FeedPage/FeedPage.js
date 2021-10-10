7120dea3351a35a980858153471d72e50cfcaa28
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-10 16:13:36 +0200

    Implementing card component

--- src/Components/FeedPage/FeedPage.js
+++ src/Components/FeedPage/FeedPage.js
@@ -2,23 +2,16 @@ import React, { useState, useEffect } from "react";
 import './styles.css'
 import { useLocation, useHistory } from 'react-router-dom'
 import getSheets from "../../Services/GetSheets";
+import SheetCard from "./SheetCard/SheetCard";
+import responseType from '../../Services/Response'
+import Loading from './Loading'
 
 function SheetsPage({ match }) {
     const [sheets, setSheets] = useState([])
+    const [response, setResponse] = useState(responseType.LOADING)
     const history = useHistory()
     const state = useLocation().state
     const handle = match.params.handle
-
-    /* 
-        If user accessed `/:handle/feed` directly
-        Go back to `/:handle` page to extract data.
-    */
-    let submissions
-    try {
-        submissions = state.submissions
-    } catch {
-        history.push(`/${handle}`)
-    }
 
     /*
         Request sheets API call 
@@ -28,9 +21,28 @@ function SheetsPage({ match }) {
         async function apiCall() {
             const t = await getSheets()
             setSheets(t)
+
+            // [todo]: handle error state.
+            setResponse(responseType.PASSED)
         }
-        apiCall()
+        /* 
+            If user accessed `/:handle/feed` directly
+            Go back to `/:handle` page to extract data.
+        */
+        let submissions
+        try {
+            submissions = state.submissions
+            apiCall()
+        } catch {
+            history.push(`/${handle}`)
+        }
     }, [])
+
+    if (response === responseType.LOADING) return <Loading />
+
+    const sheetsComponents = sheets.map((el) => {
+        return (<SheetCard key={el._id} {...el} handle={handle} />)
+    })
 
     return (
         <div className="feed-container">
@@ -41,7 +53,7 @@ function SheetsPage({ match }) {
             </div>
 
             <div className="cards">
-
+                {sheetsComponents}
             </div>
         </div>
     )
--- src/Components/FeedPage/SheetCard/SheetCard.js
+++ src/Components/FeedPage/SheetCard/SheetCard.js
@@ -1,18 +1,35 @@
 import React from "react";
 import './styles.css'
-import 'react-circular-progressbar/dist/styles.css';
 import { Link } from "react-router-dom";
 
-function SheetCard({ handle, src, name, content, author, id }) {
+function SheetCard({ _id, author, name, image, description, data, difficulty, handle }) {
+    let stars = getStars(difficulty)
     return (
-        <Link to={`/handle/${handle}/${id}`} className="card-link">
+        <Link to={`/${handle}/feed/${_id}`} className="card-link">
             <div className="card-container">
-                <h2>{name}</h2>
-                <h4>{author}</h4>
-                <p>{content}</p>
+
+                {/* <div className="image-container">
+                    <img className="image" src={image} alt={author} />
+                </div> */}
+
+                <div className="details">
+                    <span className="name">{name}</span>
+                    <span className="author">{author}</span>
+                </div>
+
+                <div className="difficulty-container">
+                    <span className="difficulty">Difficulty: {stars}</span>
+                </div>
             </div>
         </Link>
     )
+}
+
+function getStars(count) {
+    let stars = ''
+    for (let i = 0; i < count; i++)
+        stars += '⭐ '
+    return stars
 }
 
 export default SheetCard
